6d4a15fb23b1bebbf47a314a7508ac5b5d12f1c6
Alexander Maksimyak <alexander.max@gmail.com>
2016-07-06 12:32:13 +0300

    add colorpicker

--- app.rb
+++ app.rb
@@ -3,8 +3,13 @@ require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 
+@@masters = {
+			'Walter_White' => "Walter White", 
+			'Jesie_Pinkman' => "Jesie Pinkman", 
+			'Gus_Fring' => "Gus Fring"}
+
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified"			
+	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified"
 end
 
 get '/about' do
@@ -18,3 +23,24 @@ end
 get '/contacts' do
 	erb :contacts
 end
+
+post '/visit' do
+	@user_name = params[:user_name]
+	@phone = params[:phone]
+	@date_time = params[:date_time]
+	@master = params[:optionsRadios]
+	@master2 = params[:master_select]
+	@color = params[:color]
+	file_obj = File.new("./public/users.txt", "a")
+	file_obj.puts "#{@user_name};#{@phone};#{@date_time};#{@@masters[@master]}; #{@master2}"
+	file_obj.close
+	erb "#{@user_name};#{@phone};#{@date_time};#{@@masters[@master]}; #{@master2}; #{@color}"
+end
+
+post '/contacts' do
+	@users_email = params[:users_email]
+	@email_text = params[:email_text]
+	file_obj = File.new("./public/contacts.txt", "a")
+	file_obj.puts "#{@users_email}:#{@email_text}"
+	file_obj.close
+ end
